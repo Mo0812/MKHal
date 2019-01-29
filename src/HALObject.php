c656f4927ea0682362330e6e9807dbf895821b13
Moritz Kanzler <developer@moritzkanzler.de>
2019-01-29 16:27:11 +0100

    abstract functions and enable all resource types

--- src/HALObject.php
+++ src/HALObject.php
@@ -3,20 +3,40 @@ namespace MK\HAL;
 
 class HALObject implements \JsonSerializable {
 
-    public $self;
-    private $data;
+    private $self;
+    private $curies;
     private $links;
+    private $data;
     private $embedded;
 
     function __construct($self, $data = null) {
         $this->self = $self;
         $this->data = $data;
+        $this->curies = array();
         $this->links = array();
         $this->embedded = array();
     }
 
-    public function addLink(HALLink $link) {
-        $this->links[$link->label] = $link;
+    public function addCurie(HALCurie $curie) {
+        $this->curies[] = $curie;
+    }
+
+    public function getCuries() {
+        return $this->curies;
+    }
+
+    public function addLink(string $label, HALLink $link) {
+        $this->addAttributeChaining("links", $label, $link);
+    }
+
+    public function addLinkCollection(string $label, array $links) {
+        foreach($links as $link) {
+            $this->addAttributeChaining("links", $label, $link);
+        }
+    }
+
+    public function getLink($label) {
+        return $this->links[$label];
     }
 
     public function getLinks() {
@@ -24,20 +44,21 @@ class HALObject implements \JsonSerializable {
     }
 
     public function embed(string $label, HALObject $object) {
-        
-        if(isset($this->embedded[$label])) {
-            $this->embedded[$label][] = $object;
-        } else {
-            $this->embedded[$label] = array($object);
-        }
+        $this->addAttributeChaining("embedded", $label, $object);
     }
 
     public function embedCollection(string $label, array $objects) {
-        if(!empty($this->embedded[$label])) {
-            $this->embedded[$label] = array_merge($this->embedded[$label], $objects);
-        } else {
-            $this->embedded[$label] = $objects;
+        foreach($objects as $object) {
+            $this->addAttributeChaining("embedded", $label, $object);
         }
+    }
+
+    public function getEmbedded($label) {
+        return $this->embedded[$label];
+    }
+
+    public function getAllEmbedded() {
+        return $this->embedded;
     }
 
     public function exportJson() {
@@ -52,14 +73,29 @@ class HALObject implements \JsonSerializable {
                 )
             )
         );
-        foreach($this->links as $link) {
-            $hal["_links"][$link->label] = array(
-                "href" => $link->href
-            );
+        foreach($this->curies as $curie) {
+            if(isset($hal["_links"]["curies"])) {
+                $hal["_links"]["curies"][] = $curie;
+            } else {
+                $hal["_links"]["curies"] = array($curie);
+            }
+        }
+        foreach($this->links as $label => $link) {
+            $hal["_links"][$label] = $link;
         }
         foreach($this->embedded as $label => $objects) {
             $hal["_embedded"][$label] = $objects;
         }
         return $hal;
+    }
+
+    private function addAttributeChaining(string $attribute, string $label, $data) {
+        if(isset($this->$attribute[$label])) {
+            $prev_attrs = $this->$attribute[$label];
+            $prev_attrs = is_array($prev_attrs) ? $prev_attrs : array($prev_attrs);
+            $this->$attribute[$label] = array_merge($prev_attrs, array($data));
+        } else {
+            $this->$attribute[$label] = $data;
+        }
     }
 }
