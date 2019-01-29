5d6a221e7131b1be2664a020384cabaf646793db
Moritz Kanzler <developer@moritzkanzler.de>
2019-01-29 16:27:17 +0100

    update test cases

--- tests/HALTest.php
+++ tests/HALTest.php
@@ -1,34 +1,97 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use \MK\HAL;
 use MK\HAL\HALObject;
 use MK\HAL\HALLink;
+use MK\HAL\HALCurie;
 
 final class HALTest extends TestCase {
-    public function testHALCreation() {
-        $hal = new HALObject('/customer');
-        $hal->addLink(new HALLink('search', '/customer?search=?'));
+    private $hal;
 
-        $this->assertInstanceOf(HALObject::class, $hal);
-        
-        $links = $hal->getLinks();
-        $this->assertInternalType('array', $links);
-        $this->assertEquals(1, count($links));
-        $first = reset($links);
-        $this->assertInstanceOf(HALLink::class, $first);
-    }
+    public function setUp() {
+        $this->hal = new HALObject('/customer');
 
+        $this->hal->addCurie(new HALCurie('ea', 'http://example.com/docs/rels/{rel}'));
 
-    public function testHALJsonOutput() {
-        $hal = new HALObject('/customer');
-        $hal->addLink(new HALLink('search', '/customer?search=?'));
+        $this->hal->addLink('search', new HALLink('/customer?search=?'));
+
+        $this->hal->addLinkCollection('admin', array(
+            new HALLink('/admin/2'),
+            new HALLink('/admin/7')
+        ));
 
         $product = new HALObject('/product/1');
-        $product->addLink(new HALLink('next', '/product/2'));
-        $hal->embed('product', $product);
+        $product->addLink('next', new HALLink('/product/2'));
+        $this->hal->embed('product', $product);
 
-        $hal->embedCollection('coupon', array(new HALObject('/coupon/5'), new HALObject('/coupon/6')));
+        $this->hal->embedCollection('coupon', array(new HALObject('/coupon/5'), new HALObject('/coupon/6')));
+
+    }
+
+    public function testHALTypes() {
+        $hal = $this->hal;
+        $this->assertInstanceOf(HALObject::class, $hal);
+        
+        // Curies
+        $curies = $hal->getCuries();
+        $this->assertInternalType('array', $curies);
+        $this->assertEquals(1, count($curies));
+        $first = reset($curies);
+        $this->assertInstanceOf(HALCurie::class, $first);
+
+        // Links
+        $links = $hal->getLinks();
+        $this->assertInternalType('array', $links);
+        $this->assertEquals(2, count($links));
+        $first = reset($links);
+        $this->assertInstanceOf(HALLink::class, $first);
+
+        // Embedded
+        $embedded = $hal->getAllEmbedded();
+        $this->assertInternalType('array', $embedded);
+        $this->assertEquals(2, count($embedded));
+        $first = reset($embedded);
+        $this->assertInstanceOf(HALObject::class, $first);
+    }
+
+    public function testHALLinkChaining() {
+        $hal = $this->hal;
+        $hal->addLink('chaining', new HALLink('/chaining/1'));
+
+        $chaining = $hal->getLink('chaining');
+        $this->assertInstanceOf(HALLink::class, $chaining);
+
+        $hal->addLinkCollection('chaining', array(
+            new HALLink('/chaining/2'),
+            new HALLink('/chaining/3')
+        ));
+
+        $chaining = $hal->getLink('chaining');
+        $this->assertInternalType('array', $chaining);
+        $this->assertEquals(3, count($chaining));
+        $this->assertInstanceOf(HALLink::class, $chaining[1]);
+    }
+
+    public function testHALEmbeddedChaining() {
+        $hal = $this->hal;
+        $hal->embed('chaining', new HALObject('/chaining/1'));
+
+        $chaining = $hal->getEmbedded('chaining');
+        $this->assertInstanceOf(HALObject::class, $chaining);
+
+        $hal->addLinkCollection('chaining', array(
+            new HALObject('/chaining/2'),
+            new HALObject('/chaining/3')
+        ));
+
+        $chaining = $hal->getLink('chaining');
+        $this->assertInternalType('array', $chaining);
+        $this->assertEquals(2, count($chaining));
+        $this->assertInstanceOf(HALObject::class, $chaining[1]);
+    }
+
+    public function testHALJsonOutput() {
+        $hal = $this->hal;
 
         $this->assertJsonStringEqualsJsonFile(__DIR__.'/TestAssert.json', json_encode($hal));
         $this->assertJsonStringEqualsJsonFile(__DIR__.'/TestAssert.json', $hal->exportJson());
